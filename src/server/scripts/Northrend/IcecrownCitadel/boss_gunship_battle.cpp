288245484901b2289463726c20af6fce5b7270f2
Marian <britt14@gmx.de>
2012-03-25 21:37:31 +0300

    Gunship funktioniert  auch schon fast!!

--- src/server/scripts/Northrend/IcecrownCitadel/boss_gunship_battle.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_gunship_battle.cpp
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
+/* Rest des fertigen Scripts Coming soon*/
 #include "icecrown_citadel.h"
 #include "MapManager.h"
 #include "Transport.h"
